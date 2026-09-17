6fc0853f875f1b5384b191c07cabcd69af577454
Aldrin Maqyl Bin Adira <aadi0015@student.monash.edu>
2026-09-17 19:30:17 +0800

    Duplicate chart placeholders: - duplicated the two placeholder charts. - ensure charts take up its full width.

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,43 @@
 var chart_1 = "js/chart1_chart.vg.json";
+var chart_2 = "js/chart2_chart.vg.json";
+var chart_3 = "js/chart1_chart.vg.json";
+var chart_4 = "js/chart2_chart.vg.json";
+var chart_5 = "js/chart1_chart.vg.json";
+var chart_6 = "js/chart2_chart.vg.json";
+var chart_7 = "js/chart1_chart.vg.json";
+var chart_8 = "js/chart2_chart.vg.json";
+var chart_9 = "js/chart1_chart.vg.json";
+var chart_10 = "js/chart2_chart.vg.json";
+var chart_11 = "js/chart1_chart.vg.json";
+var chart_12 = "js/chart2_chart.vg.json";
+var chart_13 = "js/chart1_chart.vg.json";
+var chart_14 = "js/chart2_chart.vg.json";
 
 vegaEmbed("#chart1_chart", chart_1).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart2_chart", chart_2).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart3_chart", chart_3).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart4_chart", chart_4).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart5_chart", chart_5).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart6_chart", chart_6).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart7_chart", chart_7).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart8_chart", chart_8).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart9_chart", chart_9).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart10_chart", chart_10).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart11_chart", chart_11).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart12_chart", chart_12).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart13_chart", chart_13).then(function(result){
+}).catch(console.error);
+vegaEmbed("#chart14_chart", chart_14).then(function(result){
 }).catch(console.error);
